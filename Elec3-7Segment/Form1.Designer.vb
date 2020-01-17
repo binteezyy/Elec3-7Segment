fa9f2b1ab8d55a677bbedff3dd23d3c88aa74f89
unknown <alvinpanganiban22@gmail.com>
2020-01-18 00:41:00 +0800

    1-18-20 requirement. 7 segment counter

---
+++ Elec3-7Segment/Form1.Designer.vb
@@ -0,0 +1,172 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.A = New System.Windows.Forms.Button()
+        Me.C = New System.Windows.Forms.Button()
+        Me.B = New System.Windows.Forms.Button()
+        Me.G = New System.Windows.Forms.Button()
+        Me.E = New System.Windows.Forms.Button()
+        Me.D = New System.Windows.Forms.Button()
+        Me.F = New System.Windows.Forms.Button()
+        Me.Timer = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'A
+        '
+        Me.A.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.A.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.A.Enabled = False
+        Me.A.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.A.FlatAppearance.BorderSize = 0
+        Me.A.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.A.Location = New System.Drawing.Point(128, 43)
+        Me.A.Margin = New System.Windows.Forms.Padding(0)
+        Me.A.Name = "A"
+        Me.A.Size = New System.Drawing.Size(17, 62)
+        Me.A.TabIndex = 1
+        Me.A.UseVisualStyleBackColor = False
+        '
+        'C
+        '
+        Me.C.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.C.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.C.Enabled = False
+        Me.C.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.C.FlatAppearance.BorderSize = 0
+        Me.C.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.C.Location = New System.Drawing.Point(47, 43)
+        Me.C.Margin = New System.Windows.Forms.Padding(0)
+        Me.C.Name = "C"
+        Me.C.Size = New System.Drawing.Size(17, 62)
+        Me.C.TabIndex = 2
+        Me.C.UseVisualStyleBackColor = False
+        '
+        'B
+        '
+        Me.B.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.B.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.B.Enabled = False
+        Me.B.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.B.FlatAppearance.BorderSize = 0
+        Me.B.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.B.Location = New System.Drawing.Point(64, 25)
+        Me.B.Margin = New System.Windows.Forms.Padding(0)
+        Me.B.Name = "B"
+        Me.B.Size = New System.Drawing.Size(69, 18)
+        Me.B.TabIndex = 3
+        Me.B.UseVisualStyleBackColor = False
+        '
+        'G
+        '
+        Me.G.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.G.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.G.Enabled = False
+        Me.G.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.G.FlatAppearance.BorderSize = 0
+        Me.G.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.G.Location = New System.Drawing.Point(64, 105)
+        Me.G.Margin = New System.Windows.Forms.Padding(0)
+        Me.G.Name = "G"
+        Me.G.Size = New System.Drawing.Size(69, 18)
+        Me.G.TabIndex = 4
+        Me.G.UseVisualStyleBackColor = False
+        Me.G.Visible = False
+        '
+        'E
+        '
+        Me.E.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.E.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.E.Enabled = False
+        Me.E.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.E.FlatAppearance.BorderSize = 0
+        Me.E.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.E.Location = New System.Drawing.Point(64, 185)
+        Me.E.Margin = New System.Windows.Forms.Padding(0)
+        Me.E.Name = "E"
+        Me.E.Size = New System.Drawing.Size(69, 18)
+        Me.E.TabIndex = 7
+        Me.E.UseVisualStyleBackColor = False
+        '
+        'D
+        '
+        Me.D.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.D.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.D.Enabled = False
+        Me.D.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.D.FlatAppearance.BorderSize = 0
+        Me.D.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.D.Location = New System.Drawing.Point(47, 123)
+        Me.D.Margin = New System.Windows.Forms.Padding(0)
+        Me.D.Name = "D"
+        Me.D.Size = New System.Drawing.Size(17, 62)
+        Me.D.TabIndex = 6
+        Me.D.UseVisualStyleBackColor = False
+        '
+        'F
+        '
+        Me.F.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.F.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.F.Enabled = False
+        Me.F.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.F.FlatAppearance.BorderSize = 0
+        Me.F.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.F.Location = New System.Drawing.Point(128, 123)
+        Me.F.Margin = New System.Windows.Forms.Padding(0)
+        Me.F.Name = "F"
+        Me.F.Size = New System.Drawing.Size(17, 62)
+        Me.F.TabIndex = 5
+        Me.F.UseVisualStyleBackColor = False
+        '
+        'Timer
+        '
+        Me.Timer.Interval = 1000
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(198, 254)
+        Me.Controls.Add(Me.E)
+        Me.Controls.Add(Me.D)
+        Me.Controls.Add(Me.F)
+        Me.Controls.Add(Me.G)
+        Me.Controls.Add(Me.B)
+        Me.Controls.Add(Me.C)
+        Me.Controls.Add(Me.A)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents A As Button
+    Friend WithEvents C As Button
+    Friend WithEvents B As Button
+    Friend WithEvents G As Button
+    Friend WithEvents E As Button
+    Friend WithEvents D As Button
+    Friend WithEvents F As Button
+    Friend WithEvents Timer As Timer
+End Class
---
+++ Elec3-7Segment/Form1.vb
@@ -0,0 +1,102 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, ea As EventArgs) Handles MyBase.Load
+        Timer.Interval = 1000
+        Timer.Start()
+
+    End Sub
+
+    Private Sub Timer_Tick(sender As Object, ea As EventArgs) Handles Timer.Tick
+        Static ctr As Integer = 0
+        If ctr = 0 Then
+            ctr += 1
+            A.Visible = True
+            B.Visible = True
+            C.Visible = True
+            D.Visible = True
+            E.Visible = True
+            F.Visible = True
+            G.Visible = False
+        ElseIf ctr = 1 Then
+            ctr += 1
+            A.Visible = True
+            B.Visible = False
+            C.Visible = False
+            D.Visible = False
+            E.Visible = False
+            F.Visible = True
+            G.Visible = False
+        ElseIf ctr = 2 Then
+            ctr += 1
+            A.Visible = True
+            B.Visible = True
+            C.Visible = False
+            D.Visible = True
+            E.Visible = True
+            F.Visible = False
+            G.Visible = True
+        ElseIf ctr = 3 Then
+            ctr += 1
+            A.Visible = True
+            B.Visible = True
+            C.Visible = False
+            D.Visible = False
+            E.Visible = True
+            F.Visible = True
+            G.Visible = True
+        ElseIf ctr = 4 Then
+            ctr += 1
+            A.Visible = True
+            B.Visible = False
+            C.Visible = True
+            D.Visible = False
+            E.Visible = False
+            F.Visible = True
+            G.Visible = True
+        ElseIf ctr = 5 Then
+            ctr += 1
+            A.Visible = False
+            B.Visible = True
+            C.Visible = True
+            D.Visible = False
+            E.Visible = True
+            F.Visible = True
+            G.Visible = True
+        ElseIf ctr = 6 Then
+            ctr += 1
+            A.Visible = False
+            B.Visible = True
+            C.Visible = True
+            D.Visible = True
+            E.Visible = True
+            F.Visible = True
+            G.Visible = True
+        ElseIf ctr = 7 Then
+            ctr += 1
+            A.Visible = True
+            B.Visible = True
+            C.Visible = False
+            D.Visible = False
+            E.Visible = False
+            F.Visible = True
+            G.Visible = False
+        ElseIf ctr = 8 Then
+            ctr += 1
+            A.Visible = True
+            B.Visible = True
+            C.Visible = True
+            D.Visible = True
+            E.Visible = True
+            F.Visible = True
+            G.Visible = True
+        ElseIf ctr = 9 Then
+            ctr = 0
+            A.Visible = True
+            B.Visible = True
+            C.Visible = True
+            D.Visible = False
+            E.Visible = True
+            F.Visible = True
+            G.Visible = True
+        End If
+    End Sub
+End Class
